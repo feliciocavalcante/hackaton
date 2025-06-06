6d174f15422f37d431a2283a28d1c40a9de878f5
Adriana Cruz <drih1502@gmail.com>
2025-06-06 15:57:58 -0300

    teste console. supabase

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,6 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
-
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+console.log("SUPABASE_URL =", supabaseUrl);
+console.log("SUPABASE_ANON_KEY =", supabaseAnonKey);
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
