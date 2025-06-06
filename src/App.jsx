9ed49ebc09eb2eac44d5a15227f543b666667ee9
Adriana Cruz <drih1502@gmail.com>
2025-06-06 16:24:48 -0300

    att app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Course from './pages/Course';
-import CourseTwo from './pages/Course.two';
+import CourseTwo from './pages/CourseTwo'; // Corrigido o nome do arquivo para padrão
 import Login from './pages/Login';
 import ProtectedRoute from './components/ProtectedRoute';
 import AdminDashboard from "./pages/AdminDashboard";
@@ -13,7 +13,8 @@ function App() {
         {/* Rota pública: curso aberto para todos */}
         <Route path="/" element={<Course />} />
         
-        <Route path="/two" element={<CourseTwo />} />
+        {/* Rota para vídeo aula individual com parâmetro id */}
+        <Route path="/two/:id" element={<CourseTwo />} />
         
         <Route path="/login" element={<Login />} />
         
