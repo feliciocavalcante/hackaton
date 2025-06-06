23798c947b4803a0eaeac1397be04b3668f8cd30
Adriana Cruz <drih1502@gmail.com>
2025-06-06 14:30:48 -0300

    corrigindo o App, rotas

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Course from './pages/Course';
 import CourseTwo from './pages/Course.two';
 import Login from './pages/Login';
-import LessonPlayer from './pages/LessonPlayer';  // importe o componente novo
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
         <Route path="/" element={<Course />} />
         <Route path="/two" element={<CourseTwo />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/lesson/:id" element={<LessonPlayer />} />  {/* Rota do player */}
       </Routes>
     </BrowserRouter>
   );
